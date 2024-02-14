f64fcfcaa13558b42ab5262637a61323546aa506
kyle174 <kyoojojo@gmail.com>
2024-02-14 18:31:46 -0500

    Added PointOfInterest class #17

--- src/main/java/ca/mcmaster/se2aa4/island/team205/PhotoScanner.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team205/PhotoScanner.java
@@ -1,30 +1,19 @@
 package ca.mcmaster.se2aa4.island.team205;
 
 import java.util.List;
-import eu.ace_design.island.game.PointOfInterest;
 import java.util.ArrayList;
 
 public class PhotoScanner {
     // instance variables
-    private List <PointOfInterest> pointsOfInterest;
-    private int direction = 0;
-    
+    private final List <PointOfInterest> pointsOfInterest;
+
     public PhotoScanner(){
         this.pointsOfInterest = new ArrayList<>();
-        this.direction = 0;
     }
-    //public Photo takePhoto() {
-    //    return new Photo();
-   // }
-    public void scanTerrain(){
 
+    public void scanTerrain(){
         System.out.println("scanning terrain");
     }
 
-    public List <PointOfInterest> findPointsOfInterests(){
-
-        System.out.println("looking for POIS");
-        return pointsOfInterest;
-    }
 }
 
