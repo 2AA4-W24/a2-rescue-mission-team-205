68dc157fcd23f4eac650bb70220d431a234c1643
jjustin-k <jjkwinecki@gmail.com>
2024-02-14 19:03:20 -0500

    merging issues

--- src/main/java/ca/mcmaster/se2aa4/island/team205/PhotoScanner.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team205/PhotoScanner.java
@@ -5,26 +5,7 @@ import eu.ace_design.island.game.PointOfInterest;
 import java.util.ArrayList;
 
 public class PhotoScanner {
-    // instance variables
-    private List <PointOfInterest> pointsOfInterest;
-    private int direction = 0;
-    
-    public PhotoScanner(){
-        this.pointsOfInterest = new ArrayList<>();
-        this.direction = 0;
-    }
-    //public Photo takePhoto() {
-    //    return new Photo();
-   // }
-    public void scanTerrain(){
 
-        System.out.println("scanning terrain");
-    }
 
-    public List <PointOfInterest> findPointsOfInterests(){
-
-        System.out.println("looking for POIS");
-        return pointsOfInterest;
-    }
 }
 
