00dcd625313cc40a2dd8bfc6d073aaecca881019
jjustin-k <jjkwinecki@gmail.com>
2024-02-25 19:44:19 -0500

    Connecting classes.  There is an error somewhere in takeCommand(), will have to be fixed

--- src/main/java/ca/mcmaster/se2aa4/island/team205/Radar.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team205/Radar.java
@@ -1,17 +1,19 @@
 package ca.mcmaster.se2aa4.island.team205;
 
+import java.util.HashMap;
+
 public class Radar {
     
-    private Information info;
+    private final Information info;
 
-    public Radar(Information info){
-        this.info = info;
+    public Radar(Information information){
+        info = information;
     }
 
-    public String useRadar(Drone.Direction droneDirection) {
-        String result = "";
+    public HashMap<String, String> useRadar(Drone.Direction droneDirection) {
 
 
+        /*
         if(info.echo(droneDirection)){
             result += "Front = Ground";
         } 
@@ -34,33 +36,49 @@ public class Radar {
         } else {
             result += "Right: OUT_OF_RANGE";
         }
-
-        return result;
+        */
+        return info.echo(droneDirection);
     }
 
     // helper methods to help determine the drones relative direction
     private Drone.Direction getLeftDirection(Drone.Direction currentDirection) {
         return switch (currentDirection) {
-            case NORTH -> Drone.Direction.WEST;
-            case SOUTH -> Drone.Direction.EAST;
-            case WEST -> Drone.Direction.SOUTH;
-            case EAST -> Drone.Direction.NORTH;
+            case N -> Drone.Direction.W;
+            case S -> Drone.Direction.E;
+            case W -> Drone.Direction.S;
+            case E -> Drone.Direction.N;
         };
     }
 
     private Drone.Direction getRightDirection(Drone.Direction currentDirection) {
         return switch (currentDirection) {
-            case NORTH -> Drone.Direction.EAST;
-            case SOUTH -> Drone.Direction.WEST;
-            case WEST -> Drone.Direction.NORTH;
-            case EAST -> Drone.Direction.SOUTH;
+            case N -> Drone.Direction.E;
+            case S -> Drone.Direction.W;
+            case W -> Drone.Direction.N;
+            case E -> Drone.Direction.S;
         };
     }
 
-    public Drone.Direction chooseDirection(Drone.Direction currentDirection){
+    public String chooseDirection(Drone.Direction currentDirection){
 
-        String radarResults = useRadar(currentDirection);
-
+        HashMap<String, String> radarResults = useRadar(currentDirection);
+        for(String s : radarResults.keySet()){
+            if(radarResults.get(s).equals("GROUND")){
+               switch(s){
+                   case "Front" -> {
+                       return "Front";
+                   }
+                   case "Left" -> {
+                       return "Left";
+                   }
+                   case "Right" -> {
+                       return "Right";
+                   }
+               }
+            }
+        }
+        return "Front";
+        /*
             // if ground is detected in front, return the current direction
         if (radarResults.contains("Front = Ground")) {
             return currentDirection;
@@ -78,6 +96,8 @@ public class Radar {
 
         // If no ground detected fly forward
         return currentDirection;
+        */
+
     }
 }
 
