0900153b3c626504bf99d30a2e9f770cd95d0c34
kyle174 <kyoojojo@gmail.com>
2024-02-29 22:47:08 -0500

    fixed the coverage issue on sonarqube #62

--- src/test/java/PhotoScannerTest.java
+++ src/test/java/PhotoScannerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class PhotoScannerTest {
+class PhotoScannerTest {
 
     private PhotoScanner photoScanner;
     private UsingJSON usingJSON;
