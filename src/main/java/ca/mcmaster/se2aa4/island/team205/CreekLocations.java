f3226ca69332eb38c8eb4c27bb7a88b982ddd373
jjustin-k <jjkwinecki@gmail.com>
2024-03-01 16:06:36 -0500

    Started working on figuring out how to track drones location.  This location traking will be done in movement as movement will update the location of the drone each time it moves.  Also created a new class CreekLocations, this is where we will store our identifiers of the creeks.  Made changes to PointsOfIntrests to interact with CreekLocations.  Also started the bones of the GridSearch, with the logic to be implemented later.

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team205/CreekLocations.java
@@ -0,0 +1,29 @@
+package ca.mcmaster.se2aa4.island.team205;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CreekLocations {
+
+    private final Map<PointOfInterest, int[]> creeks = new HashMap<>();
+
+
+    public void addCreek(PointOfInterest poi){
+        creeks.put(poi,  poi.location);
+    }
+
+    public PointOfInterest closestCreak(PointOfInterest site){
+        int minimum = 1000;
+        PointOfInterest closest = null;
+        for(PointOfInterest poi : creeks.keySet()){
+            int distanceToSite = Math.abs(poi.location[0] -  site.location[0]) + Math.abs(poi.location[1] -  site.location[1]);
+            if(distanceToSite <= minimum){
+                minimum = distanceToSite;
+                closest = poi;
+            }
+        }
+        return closest;
+    }
+
+
+}
