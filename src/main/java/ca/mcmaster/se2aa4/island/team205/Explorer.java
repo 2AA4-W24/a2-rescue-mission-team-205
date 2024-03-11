05dedaa2fa12030a349988c2130235e4137ef839
kyle174 <kyoojojo@gmail.com>
2024-03-11 16:52:02 -0400

    refactored a few classes to get rid of code smells #99

--- src/main/java/ca/mcmaster/se2aa4/island/team205/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team205/Explorer.java
@@ -18,17 +18,14 @@ public class Explorer implements IExplorerRaid {
 
     @Override
     public void initialize(String s) {
-
         center = new CommandCenter(s);
-
-       // logger.info("** Initializing the Exploration Command Center");
+        logger.info("** Initializing the Exploration Command Center");
         JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
-        //String start = info.toString(2);
-       // logger.info("** Initialization info:\n {}",start);
-       // Integer batteryLevel = info.getInt("budget");
-
-       // logger.info("The drone is facing {}", center);
-        //logger.info("Battery level is {}", batteryLevel);
+        String start = info.toString(2);
+        logger.info("** Initialization info:\n {}",start);
+        Integer batteryLevel = info.getInt("budget");
+        logger.info("The drone is facing {}", center);
+        logger.info("Battery level is {}", batteryLevel);
     }
 
     @Override
@@ -36,7 +33,6 @@ public class Explorer implements IExplorerRaid {
         center.takeCommand();
         String decision = center.decision();
         logger.info("** Decision: {}",decision);
-
         return center.decision();
     }
 
@@ -46,9 +42,9 @@ public class Explorer implements IExplorerRaid {
         JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
         logger.info("** Response received:\n"+response.toString(2));
         Integer cost = response.getInt("cost");
-       // logger.info("The cost of the action was {}", cost);
+        logger.info("The cost of the action was {}", cost);
         String status = response.getString("status");
-       // logger.info("The status of the drone is {}", status);
+        logger.info("The status of the drone is {}", status);
         JSONObject extraInfo = response.getJSONObject("extras");
         logger.info("Additional information received: {}", extraInfo);
         logger.info(center.getRange());
@@ -56,7 +52,6 @@ public class Explorer implements IExplorerRaid {
 
     @Override
     public String deliverFinalReport() {
-
         logger.info("final Report: " + center.finalReport());
         return center.finalReport();
     }
--- src/main/java/ca/mcmaster/se2aa4/island/team205/UsingJSON.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team205/UsingJSON.java
@@ -1,9 +1,5 @@
 package ca.mcmaster.se2aa4.island.team205;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.json.JSONArray;
-
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
